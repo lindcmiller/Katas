3ba081d62b2528b036c0f78ff03029ae2de4853c
Lindsey Miller <lindc.miller@gmail.com>
2015-08-11 15:14:28 -0400

    clean up result repetition

--- coin_changer.rb
+++ coin_changer.rb
@@ -3,16 +3,6 @@ class CoinChanger
 		[25,10,5,1].inject([]) do |result, coin|
 			result + coins_for_amount(coin, remaining_amount(amount, result))
 		end
-		result = []
-			
-		result = result + coins_for_amount(25, remaining_amount(amount, result))
-			
-		result = result + coins_for_amount(10, remaining_amount(amount, result))
-		
-		result = result + coins_for_amount(5, remaining_amount(amount, result))
-		
-		result = result + coins_for_amount(1, remaining_amount(amount, result))
-		result
 	end
 	
 	private
