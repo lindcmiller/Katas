cb40d36480422dbd8a84ecf1c98876d7d3467bc5
Lindsey Miller <lindc.miller@gmail.com>
2015-08-11 16:11:43 -0400

    fix typos to pass tests

--- changer_spec.rb
+++ changer_spec.rb
@@ -1,4 +1,4 @@
-require 'coin_changer'
+require './coin_changer'
 
 describe CoinChanger do
 	let(:changer) { CoinChanger.new }
@@ -9,7 +9,7 @@ describe CoinChanger do
 		expect(changer.make_change_for(1)).to eq [1]
 	end 
 	it "gives a pennies for pennies" do	
-		expect(changer.maker_change_for(2)).to eq [1,1]
+		expect(changer.make_change_for(2)).to eq [1,1]
 	end 
 	it "gives nickels for 5 cents" do
 		expect(changer.make_change_for(5)).to eq [5]
@@ -24,7 +24,7 @@ describe CoinChanger do
 		expect(changer.make_change_for(25)).to eq [25]
 	end	
 	it "gives lots of change for close to a dollar" do
-		expect(change.make_change_for(99)).to eq [25,25,25,10,10,1,1,1,1]
+		expect(changer.make_change_for(99)).to eq [25,25,25,10,10,1,1,1,1]
 	end	
 	it "gives quarters for a dollar" do 
 		expect(changer.make_change_for(100)).to eq [25,25,25,25]
