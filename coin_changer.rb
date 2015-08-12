681ac5deb0efc612883a6963a0e97c91b8fa5bfc
Josh Van Cleef <josh@joshvc.com>
2015-08-12 14:27:05 -0400

    pairing coin changer exercise

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,18 +1,12 @@
-class CoinChanger 
-	def make_change_for(amount)
-		[25,10,5,1].inject([]) do |result, coin|
-			result + coins_for_amount(coin, remaining_amount(amount, result))
-		end
-	end
-	
-	private
-	
-	def remaining_amount(initial_amount, coins_so_far)
-		initial_amount - (coins_so_far.inject(&:+) || 0)
-	end	
-	
-	def coins_for_amount(coin, amount)
-		coins = amount / coin
-		coins.times.map { coin }	
-	end	
-end 	
+class CoinChanger
+  def make_change_for(amount)
+    result = Array.new
+    remainder = amount
+    [50, 25, 10, 5, 1].each do |coin|
+      quantity = remainder / coin
+      remainder = remainder % coin
+      quantity.times { result << coin }
+    end
+    return result
+  end
+end
