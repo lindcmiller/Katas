45e7709b3fc023592bcd2be01596b0c52294ab13
Lindsey Miller <lindc.miller@gmail.com>
2015-08-20 13:50:48 -0400

    harder to follow solution

---
+++ coin_8.20.15/coin_changer.rb
@@ -0,0 +1,18 @@
+class CoinChanger
+  result = []
+  def make_change_for(amount)
+    [25,10,5,1].inject([]) do |result, coin|
+      result + coins_for_amount(coin, remaining_amount(amount, result))
+    end
+  end
+
+  private
+
+  def remaining_amount(initial_amount, coins_so_far)
+    initial_amount - (coins_so_far.inject(&:+) || 0)
+  end
+
+  def coins_for_amount(coin, amount)
+    coins = amount / coin
+  end
+end
